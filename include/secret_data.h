648aa328e4dc20b138ff14667f4bedb05acbbc8d
LucaDal <dalessandroluca1@gmail.com>
2025-04-08 22:45:08 +0200

    added encripted connection to broker

--- include/secret_data.h
+++ include/secret_data.h
@@ -3,4 +3,8 @@
 #define SERVER_ADDRESS "lucadalessandro.freeddns.org"
 #define TOKEN_ID "TEMP_IGRO_01"
 
+const char *mqtt_broker = "emqx.lucadalessandro.freeddns.org";
+const int mqtt_port = 8883;
+String mqtt_topic = "domotica/mirandola/camera/temp_igro/";
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,38 @@
 #include "secret_data.h"
 #include "AuthenticationManager.h"
 #include <Wire.h>
-//#include "DFRobot_SHT20.h"
 #include "PubSubClient.h"
 #include "SHTSensor.h"
+#include "cert.h"
 
-#define DEEPSLEEP 300e6 //5 minuti
-#define UPDATE_TIME 300000 //5 minuti
+#define DEEPSLEEP 600e6 //10 minuti
+#define UPDATE_TIME 600e6 //10 minuti
 
 SimpleOTA *simpleOTA = new SimpleOTA();
 JsonDocument doc;
 
 const char * mqtt_username;
 const char * mqtt_password;
-String mqtt_topic = "domotica/mirandola/camera/temp_igro/";
-const int mqtt_port = 1883;
 
-WiFiClient espClient;
-PubSubClient mqtt_client(espClient);
+BearSSL::WiFiClientSecure esp_client;
+PubSubClient mqtt_client(esp_client);
 
 SHTSensor sht;
 
 unsigned long update_time = 0;
 
 void connectToMQTTBroker() {
-
   int n_try = 5;
   while (!mqtt_client.connected() && n_try > 0) {
       Serial.println("Connecting...");
       String client_id = "esp8266-" + String(WiFi.macAddress());
-      if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-          Serial.println("Connected to MQTT broker");
+      if (mqtt_client.connect(client_id.c_str(),mqtt_username, mqtt_password)) {
+          //Serial.println("Connected to MQTT broker");
           mqtt_client.subscribe(mqtt_topic.c_str());
           break;
       } else {
-          Serial.println("Failed connecting - retrying in 5 seconds");
+          //Serial.println("Failed connecting - retrying in 5 seconds");
+          //Serial.println(mqtt_client.state());
           delay(5000);
       }
       n_try--;
@@ -47,44 +45,49 @@ void connectToMQTTBroker() {
 }
 
 void update(){
-
   if(millis() > update_time){
     update_time = millis() + UPDATE_TIME;
     sht.readSample();
     doc["temp"] = String(sht.getTemperature(),1);
-    doc["umid"] = String(sht.getHumidity(),0); 
+    doc["umid"] = String(sht.getHumidity(),0);     
+    // doc["temp"] = String(20.9,1);
+    // doc["umid"] = String(89.55,0); 
     mqtt_client.publish(mqtt_topic.c_str(),doc.as<String>().c_str());
-    delay(200);
+    delay(500);
     if(DEEPSLEEP > 0)
-       ESP.deepSleep(DEEPSLEEP);
+      ESP.deepSleep(DEEPSLEEP);
   }
 }
 
 void setup() {
-
+  //Serial.begin(115200);
   Wire.begin(2, 0);  //sda - scl
   sht.init();
   AuthManger am((size_t)0, (size_t)512);
-  
+
   mqtt_username = am.getUserame();
   mqtt_password = am.getPassword();
- 
   mqtt_topic.concat(mqtt_username);
+  
+  // Serial.printf("[%s]\n",mqtt_password);
+  // Serial.printf("[%s]\n",mqtt_password);
 
   WiFiManager wifiManager;
   wifiManager.setConfigPortalTimeout(120);
   wifiManager.autoConnect("TempIgro");
-
-  if (WiFi.status() == WL_CONNECTED){
-    simpleOTA->begin(512, SERVER_ADDRESS, TOKEN_ID, true);
-  }
-
-  mqtt_client.setServer(SERVER_ADDRESS, mqtt_port);
+  
+  simpleOTA->begin(512, SERVER_ADDRESS, TOKEN_ID, true);
+  
+  BearSSL::X509List *serverTrustedCA = new BearSSL::X509List(ssl_ca_cert);
+  esp_client.setTrustAnchors(serverTrustedCA);
+  esp_client.setBufferSizes(512, 512);
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+  
   connectToMQTTBroker();
 }
 
 void loop() {
-  simpleOTA->checkUpdates(300);
+  //simpleOTA->checkUpdates(86400);//24 ore
   if (!mqtt_client.connected()) {
       connectToMQTTBroker();
   }else{
